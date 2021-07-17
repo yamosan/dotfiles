57f167d73fc97b03086cbc99b8ef14c6319dce05
yamori-masato <yamorimasato@gmail.com>
2021-07-18 02:42:13 +0900

    add vscode

---
+++ vscode/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh 
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+ln -svf "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+
+install extention
+cat extensions | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > extensions
