b9807137742abe165cccca7689bd048a3b74e2fe
yamosan <yamorimasato@gmail.com>
2022-02-22 21:54:08 +0900

    fix

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ DOT_FILES=(.zshrc .starship .gitconfig .gitignore_global .hammerspoon)
 DOT_PATH=$HOME/dotfiles
 
 for file in ${DOT_FILES[@]}; do
-  ln -nsvf $DOT_PATH/$file $HOME/$file
+  ln -svf $DOT_PATH/$file $HOME/$file
 done
